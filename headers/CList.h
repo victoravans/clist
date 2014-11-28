0799b5cdb54f8ed66ef401b02fd2e661841fa03b
Percee <percee@live.fr>
2014-11-28 10:21:51 +0100

    update CList.h

--- headers/CList.h
+++ headers/CList.h
@@ -1,7 +1,8 @@
 #ifndef __CLIST_H__
 #define __CLIST_H__
 
-#include <cstddef>
+#include <cstddef>		//size_t
+#include <memory>		//shared_ptr
 #include "CNode.h"
 
 namespace nsSdD
@@ -10,13 +11,24 @@ template <class T>
 class CList
 {
 private:
-	CNode<T> m_Head;
-	CNode<T> m_Tail;
+	std::shared_ptr <CNode<T>> m_Head;	//head sentinel
+	std::shared_ptr <CNode<T>> m_Tail;	//tail sentinel
+
 public:
-	explicit CList ();
-	explicit CList (std::size_t n);
+	explicit CList ();							//empty list
+	explicit CList (std::size_t n);				//
 	CList (std::size_t n, const T& val);
 	CList (const CList& x);
+
+	CList& operator= (const CList& l);
+	void assign (std::size_t n, const T& val);	//fill the list with n elements, each a copy of val
+
+	bool empty ();		//returns true if the list is empty
+	size_t size ();		//returns the number of elements of the list
+
+	CNode<T>&	front ();				//reference to the first element
+	const CNode<T>&	front () const;		//const reference to the first element
+
 };
 }
 
