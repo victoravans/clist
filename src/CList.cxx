6833c2d67a08405609e451bfdb15009c6e2d4e66
Percee <percee@live.fr>
2014-11-28 11:18:05 +0100

    created empty function definitions in CList.cxx

--- src/CList.cxx
+++ src/CList.cxx
@@ -1,26 +1,148 @@
 #include "CList.h"
 
 
+#define CLIST nsSdD::CList<T>
+
 template <class T>
-nsSdD::CList<T>::CList ()
+CLIST::CList ()
 {
 
 }
 
 template <class T>
-nsSdD::CList<T>::CList (std::size_t n)
+CLIST::CList (std::size_t n)
 {
 
 }
 
 template <class T>
-nsSdD::CList<T>::CList (std::size_t n, const T& val)
+CLIST::CList (std::size_t n, const T& val)
 {
 
 }
 
 template <class T>
-nsSdD::CList<T>::CList (const CList& x)
+CLIST::CList (const CList& x)
+{
+
+}
+
+template <class T>
+CLIST& CLIST::operator= (const CList& l)
+{
+
+}
+
+template <class T>
+void CLIST::assign (std::size_t n, const T& val)
+{
+
+}
+
+template <class T>
+bool CLIST::empty ()
+{
+
+}
+
+template <class T>
+size_t CLIST::size ()
+{
+
+}
+
+template <class T>
+CNode<T>& CLIST::front ()
+{
+
+}
+
+template <class T>
+const CNode<T>&	CLIST::front () const
+{
+
+}
+
+template <class T>
+CNode<T>& CLIST::back ()
+{
+
+}
+
+template <class T>
+const CNode<T>&	CLIST::back() const
+{
+
+}
+
+template <class T>
+void CLIST::push_front (const T& val)
+{
+
+}
+
+template <class T>
+void CLIST::push_back (const T& val)
+{
+
+}
+
+template <class T>
+void CLIST::pop_front()
+{
+
+}
+
+template <class T>
+void CLIST::pop_back()
+{
+
+}
+
+template <class T>
+void CLIST::swap (CList& l)
+{
+
+}
+
+template <class T>
+void CLIST::resize (std::size_t n, T val = T())
+{
+
+}
+
+template <class T>
+void CLIST::clear()
+{
+
+}
+
+template <class T>
+void CLIST::remove (const T& val)
+{
+
+}
+
+template <class T>
+void CLIST::unique()
+{
+
+}
+
+template <class T>
+void CLIST::merge (CList& l)
+{
+
+}
+
+template <class T>
+void CLIST::sort()
+{
+
+}
+
+template <class T>
+void CLIST::reverse()
 {
 
 }
