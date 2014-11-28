a1fce22d33c575f59fff34d0d58b8d36bad41e81
Elekhyr <thomas.margier@live.fr>
2014-11-28 09:10:31 +0100

    thomas-cunt commit

---
+++ src/CNode.cxx
@@ -0,0 +1,18 @@
+/*
+ * CNode.cpp
+ *
+ *  Created on: 28 nov. 2014
+ *      Author: m13009698
+ */
+
+#include "CNode.h"
+
+CNode::CNode() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CNode::~CNode() {
+	// TODO Auto-generated destructor stub
+}
+
