9bd38c8f63dc9656bde0f286c345c37e3ae2457c
Percee <percee@live.fr>
2014-11-28 09:01:53 +0100

    update CList.h

--- headers/CList.h
+++ headers/CList.h
@@ -1,10 +1,13 @@
 #include <cstddef>
+#include "CNode.h"
 
 namespace nsSdD
 {
 template <class T>
 class CList
 {
+	CNode m_Head;
+	CNode m_Tail;
 	explicit CList ();
 	explicit CList (std::size_t n);
 	CList (std::size_t n, const T& val);
