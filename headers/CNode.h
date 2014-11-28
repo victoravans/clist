4fab9e42b14a83c9952f5341e243afa0bcae3a65
Elekhyr <thomas.margier@live.fr>
2014-11-28 09:14:02 +0100

    add cnode.h

---
+++ headers/CNode.h
@@ -0,0 +1,31 @@
+/*
+ * CNode.h
+ *
+ *  Created on: 28 nov. 2014
+ *      Author: m13009698
+ */
+
+#ifndef CNODE_H_
+#define CNODE_H_
+
+template <typename T>
+class CNode {
+
+	typedef CNode <T> * PCNode;
+
+	T m_info;
+	PCNode p_Suivant;
+	PCNode p_Precedent;
+
+public:
+	CNode();
+	virtual ~CNode();
+
+	T getInfo();
+	PCNode getSuivant();
+	PCNode getPrecedent();
+
+	void
+};
+
+#endif /* CNODE_H_ */
