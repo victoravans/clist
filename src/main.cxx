ee5d4dda690b7341421e23f8ac41d28a0f8de230
Percee <percee@live.fr>
2014-12-04 20:17:01 +0100

    create project visual studio

--- src/main.cxx
+++ src/main.cxx
@@ -6,13 +6,5 @@ using namespace std;
 
 int main ()
 {
-	CList <int> a,b;
-	for (unsigned i = 1; i <= 5; ++i)
-		a.push_back (i);
-	for (unsigned i = 10; i <= 12; ++i)
-		b.push_back(i);
-	a.AfficherList();
-	cout << endl;
-	a = b;
-	a.AfficherList();
+	CList <int> a;
 }
